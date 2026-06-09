09032383c5515d0525bc932cf5e55c0ccdc835ce
Mahmoud <Mahmoud.kyd001@gmail.com>
2026-06-09 15:20:53 +0200

    Baumfällantrag YAML hinzugefügt

---
+++ baum-service/rest/getBaumfaellungForm.go
@@ -0,0 +1,44 @@
+// Edit this file, as it is a specific handler function for your service
+package rest
+
+import (
+	"baumservice/core/log"
+	"baumservice/core/tracing"
+	"baumservice/web/pages"
+
+	"github.com/labstack/echo/v5"
+)
+
+// Baumfällantrag anzeigen
+func GetBaumfaellungForm(c *echo.Context) error {
+	// trace span
+	ctx := (*c).Request().Context()
+	ctx, span := tracing.Tracer.Start(ctx, "logMessage")
+	defer span.End()
+
+	traceId := span.SpanContext().TraceID().String()
+	spanId := span.SpanContext().SpanID().String()
+	log.Info().Str("traceId", traceId).Str("spanId", spanId).Str("path", (*c).Request().URL.Path).Msg("GetBaumfaellungForm")
+
+	/* Remove this comment to use session
+	session, err := getSession(c)
+	if err != nil {
+		log.Error().Err(err).Msg("GetBaumfaellungForm failed")
+	 	return (*c).NoContent(http.StatusInternalServerError)
+	}
+	*/
+
+	/* Remove this comment to use localization
+	lzr := i18n.NewLocalizer(core.Bundle, core.Language(c))
+	*/
+
+	log.Debug().
+		Msg("Received request for GetBaumfaellungForm, running business logic")
+
+	/*
+	 * Business Logic
+	 */
+
+	// 200 => Formular anzeigen
+	return pages.BaumfaellungForm().Render((*c).Request().Context(), (*c).Response())
+}
